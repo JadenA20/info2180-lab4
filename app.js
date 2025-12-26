ba7d2b6852b72f546742d9c14bc51874575b08bd
Jaden <jaden.anthony2@gmail.com>
2025-12-26 13:41:24 -0500

    Update app.js

--- app.js
+++ app.js
@@ -1,5 +1,29 @@
 document.addEventListener("DOMContentLoaded", function(){
 
-  const lookupButton = document.getElementById("search");
+  const searchButton = document.getElementById("button");
+  const queryResult = document.getElementById("result");
+  const userInput = document.getElementById("search");
 
-}
+  //AJAX Request
+  function superheroList(){
+  
+    const userRequest = new XMLHttpRequest();
+    const userQuery = userInput.value
+
+    userRequest.open('GET', `superheroes.php?query=${encodeURIComponent(userQuery)}`, true);
+    userRequest.onreadystatechange = function() {
+      if (userRequest.readyState === XMLHttpRequest.DONE) {
+        if (userRequest.status === 200) {
+          let response = userRequest.responseText;
+          queryResult.innerHTML = response;
+        }
+        else {
+          alert('There was an issue with the request receieved');
+        }
+      }
+    }
+    userRequest.send()
+  };
+
+ searchButton.addEventListener('click', superheroList);
+)};
